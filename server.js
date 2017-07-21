aaf947d2886506d5c481f279eb21a4b63b4da520
clivemchd <clivemchd@gmail.com>
2017-07-21 17:26:26 +0530

    Defined first folder structure created package.json Added .gitignore Installed necessary node_module dependencies Created server.js file Hosted a Server on localhost : 9000 created index.html in public folder

---
+++ server.js
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////
+/* This is the starting point of the application
+/////////////////////////////////////////////////////
+
+/**
+ * Required Express web application framework and created a instance in variable app
+ */
+var express			=	require('express')
+var app 				= express()
+
+/**
+ * Middleware to serve static files in the Public folder
+ */
+app.use(express.static(__dirname + '/public'))
+
+/**
+ * Server listneing on port 9000
+ * @param 	INT 9000 		[Port Number]
+ * @param 	Callback    [Consoles the port at which Server is created]
+ */
+app.listen(9000, () => {
+	console.log('\x1b[36m','Server listening on port','\x1b[1m', '\x1b[37m', '9000', '\x1b[0m')
+	console.log(' Server Open at : ', '\x1b[33m', '\x1b[1m', 'https://localhost:9000', '\x1b[0m')
+})
